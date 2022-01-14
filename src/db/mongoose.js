922013d68f739abd348b3ef81f2cc272bf8bbf6d
nic-inphosoft <97273140+nic-inphosoft@users.noreply.github.com>
2022-01-14 13:31:59 +0700

    Update mongoose.js

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose')
-
+//PORT=3000
+// SENDGRID_API_KEY=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
+// MONGODB_URL=mongodb://127.0.0.1:27017/task-manager-api
+// JWT_SECRET=thisismynewcourse
 mongoose.connect(process.env.MONGODB_URL,{
     useNewUrlParser: true,
     useCreateIndex: true,
